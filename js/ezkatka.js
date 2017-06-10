8519f4be95a77a452708bca1ddec3abf22c8d4cb
SpyroSlayer <johnhicks929@gmail.com>
2017-06-10 18:34:51 +0100

    Clash Royale

--- js/ezkatka.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$('.offerList').hide();
-
-});
-
-function loadOffers(){
-	$('.loaderOffer').slideUp(1000);
-	setTimeout(function(){$('.offerList').slideDown(1000)}, 1000);
-}
